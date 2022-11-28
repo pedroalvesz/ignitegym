801eedd8dbdcb0f23e4012e1834b148fc8298c3c
Pedro Henrique <pedro.zacarias1@aluno.unip.br>
2022-11-28 14:20:48 -0300

    Customizing Bottom Tabbar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native'
+import { useTheme } from 'native-base'
 import {createBottomTabNavigator, BottomTabNavigationProp} from '@react-navigation/bottom-tabs'
 
 const {Navigator, Screen} = createBottomTabNavigator<AppRoutes>()
@@ -7,6 +9,9 @@ import { History } from '@screens/History'
 import { Profile } from '@screens/Profile'
 import { Exercise } from '@screens/Exercise'
 
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
 
 type AppRoutes = {
   dashboard: undefined;
@@ -18,12 +23,31 @@ type AppRoutes = {
 export type AppRoutesNavigationProps = BottomTabNavigationProp<AppRoutes>;
 
 export function AppRoutes() {
+
+  const {sizes, colors} = useTheme()
+
+  const iconSize = sizes[8]
+
   return(
-    <Navigator>
-      <Screen name="dashboard" component={Dashboard}/>
-      <Screen name="history" component={History}/>
-      <Screen name="profile" component={Profile}/>
-      <Screen name="exercise" component={Exercise}/>
+    <Navigator
+    screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: colors.green[500],
+      tabBarInactiveTintColor: colors.gray[200],
+      tabBarStyle: {
+        backgroundColor: colors.gray[600],
+        borderTopWidth: 0,
+        height: Platform.OS === 'android' ? 'auto' : 96,
+        paddingTop: sizes[6],
+        paddingBottom: sizes[10],
+      }
+    }}
+    >
+      <Screen name="dashboard" component={Dashboard} options={{tabBarIcon : ({ color }) => <HomeSvg fill={color} height={iconSize} width={iconSize} />}}/>
+      <Screen name="history" component={History} options={{tabBarIcon: ({color}) => <HistorySvg fill={color} height={iconSize} width={iconSize} />}}/>
+      <Screen name="profile" component={Profile} options={{tabBarIcon : ({color}) => <ProfileSvg fill={color} height={iconSize} width={iconSize} /> }}/>
+      <Screen name="exercise" component={Exercise} options={{tabBarButton: () => null}}/>
     </Navigator>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Box, useTheme} from 'native-base'
 import { NavigationContainer, DefaultTheme} from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from './app.routes';
 
 export function Routes() {
 
@@ -8,10 +9,12 @@ export function Routes() {
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
 
+  //basicamente trocando o nome do DefaultTheme do Navigation para adicionar uma cor que já tenho no meu tema
+
   return(
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes/>
+        <AppRoutes/>
       </NavigationContainer>
     </Box>
   )
